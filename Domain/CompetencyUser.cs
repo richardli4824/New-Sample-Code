6117d1b0a5a9bf452a59db2f5bb2e58a896f5214
Richard <richardli4824@gmail.com>
2018-04-01 23:19:43 -0700

    Add files via upload

---
+++ Domain/CompetencyUser.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PLC.Domain
+{
+    public class CompetencyUser
+    {
+        public string Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int CompetencyId { get; set; }
+        public string CompetencyName { get; set; }
+        public string Email { get; set; }
+        public string AuthorizedBy { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public string Status { get; set; }
+        public string ActivityType { get; set; }
+        public string Notes { get; set; }
+        public string Attachments { get; set; }
+        public int competencyUserId { get; set; }
+
+
+    }
+}
---
+++ RequestModel/AdminCompetencyRequestModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PLC.Models.Requests
+{
+    public class AdminCompetencyRequestModel
+    {
+        public string UserId { get; set; }
+        public int CompetencyId { get; set; }
+        public string AuthorizedBy { get; set; }
+        public string Status { get; set; }
+        public string Notes { get; set; }
+        public string ActivityType { get; set; }
+        public string Attachment { get; set; } 
+        public int competencyUserId { get; set; }
+        public string Id { get; set; }
+
+    }
+
+}
